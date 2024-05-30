0f932059a0d115baf94523ccca90864549135138
Nic98 <yzzon@student.unimelb.edu.au>
2024-05-30 17:36:29 +1000

    upload

---
+++ .history/sidebar_20240527164142.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#btn");
+
+
+function expandSideBar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("active");
+}
---
+++ sidebar.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector("#btn");
+function expandSideBar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("active");
+}
